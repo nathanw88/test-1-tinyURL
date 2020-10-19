285953b42a99419eba67b04a15e7be7ce55ccd9f
Nate <nathanw88@gmail.com>
2020-10-19 18:06:16 -0500

    made it look for react app when now api route hit

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,9 @@ const apiRoutes = require("./api");
 router.use("/api", apiRoutes);
 
 
+// If no API routes are hit, send the React app
 router.use((req, res) =>
-  res.status(404).json({ message: "Route not found" })
+  res.sendFile(path.join(__dirname, "../client/build/index.html"))
 );
 
 module.exports = router;
